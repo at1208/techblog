2303fbfc1689e3b96c4a063e77ca7edf93fa0349
at1208 <amantiwari12081999@gmail.com>
2019-11-06 12:53:36 +0530

    icons added to drawer

--- src/components/Drawer/drawer.js
+++ src/components/Drawer/drawer.js
@@ -34,15 +34,15 @@ class SideDrawer extends Component {
           visible={this.state.visible}
           >
           <div className='text-center'>
-          <button className='text-center btn btn-block   shadow b3'>Phone Reviews</button>
-          <button className='text-center btn btn-block   shadow b3'>New Launches</button>
-          <button className='text-center btn btn-block   shadow b3'>Upcoming Phones</button>
-          <button className='text-center btn btn-block   shadow b3'>Gaming Section</button>
-          <button className='text-center btn btn-block   shadow b3'>Headphones & Earphones</button>
-          <button className='text-center btn btn-block   shadow b3'>Laptops</button>
-          <button className='text-center btn btn-block   shadow b3'>Leaks & Rumors</button>
-          <button className='text-center btn btn-block   shadow b3'>Facts & Figures</button>
-          <button className='text-center btn btn-block   shadow b3'>Best Buy Links</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="mobile" className='float-left b4'/>Phone Reviews</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="thunderbolt" className='float-left b5'/>New Launches</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="appstore" className='float-left b6'/>Upcoming Phones</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="robot" className='float-left b7'/>Gaming Section</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="customer-service" className='float-left b8'/>Headphones & Earphones</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="laptop" className='float-left b9'/>Laptops</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="fire" className='float-left b10'/>Leaks & Rumors</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="read" className='float-left b11'/>Facts & Figures</button>
+          <button className='  btn btn-block   shadow b3'><Icon type="global" className='float-left b12'/>Best Buy Links</button>
           </div>
         </Drawer>
       </div>
