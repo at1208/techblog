b67c93892267ae089583d0e7c1e64c0683f07dc8
at1208 <amantiwari12081999@gmail.com>
2019-11-06 09:20:37 +0530

    jc

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,10 +5,10 @@ import Drawer from '../Drawer/drawer';
 
 const Header = () => {
   return <div className='container-fluid a1'>
-  <span>
+
                <Drawer/>
-               <h1>GeekBoy</h1>
-                        </span>
+               
+
          </div>
 
 }
