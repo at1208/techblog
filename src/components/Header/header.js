82c63302370e269f4763c724c4ca2ae6298a365b
at1208 <amantiwari12081999@gmail.com>
2019-11-06 09:19:15 +0530

    jc

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './header.css'
 import Drawer from '../Drawer/drawer';
- 
+
 
 const Header = () => {
   return <div className='container-fluid a1'>
-
+  <span>
                <Drawer/>
+               <h1>GeekBoy</h1>
+                        </span>
          </div>
+
 }
 export default Header;
