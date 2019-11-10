de3a37832dc2874a32a22273bd0e5086648732ea
at1208 <amantiwari12081999@gmail.com>
2019-11-10 18:37:49 +0530

    some changed

--- src/components/LeaksRumors/Deletingfb/deletingfbDetail.js
+++ src/components/LeaksRumors/Deletingfb/deletingfbDetail.js
@@ -8,13 +8,12 @@ const DeletingfbDetail = () => {
   <div className='container c21'>
   <h5 className='e5'>Brian Acton still behind the idea of deleting Facebook</h5>
 
+<img src='../../../blog7.jpg' className='e1'/>
   <p className='e3'>At Wired’s 25th anniversary summit, WhatsApp co-founder Brian Acton told the crowd that, while individuals need to settle on their own choices about the social network, he remains by his choice to leave. “If you want to be on Facebook and you want to have ads thrust in front of you, go to town”, he added.</p>
 
-  <p className='e3'>Three new adult-sized electric bicycles are slated to roll off the production lines in 2020, bringing one of the oldest motorcycle companies in the world firmly into the electric vehicle age.
-</p>
 <p className='e3'>Acton’s original tweet came at the stature of the Cambridge Analytica embarrassment, as the #DeleteFacebook movement swept the state. The organisation was reeling from disclosures in regards to the political counselling firm’s mishandling of user’s information with the particular aim of affecting the 2016 political election, when Acton bounced into the conflict.
 </p>
-  <img src='../../../blog7.jpg' className='e1'/>
+
 
 <p className='e3'>At the time, Acton had been away from Facebook for over a year, venturing down in 2017 over a conflict with Mark Zuckerberg in regards to WhatsApp's monetisation. He left millions on the table in unvested stock and turned into a vocal critic of the social network.
 
