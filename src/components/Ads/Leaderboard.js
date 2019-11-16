dd61ae579c88fab6a8173b95956add19a83e4b4e
at1208 <amantiwari12081999@gmail.com>
2019-11-16 07:15:57 +0530

    .

--- src/components/Ads/Leaderboard.js
+++ src/components/Ads/Leaderboard.js
@@ -5,7 +5,7 @@ class Leaderboard extends Component {
   componentDidMount(){
     try {
               window._mNHandle.queue.push(function (){
-                  window._mNDetails.loadTag("925165617", "720x90", "925165617");
+                  window._mNDetails.loadTag("925165617", "728x90", "925165617");
               });
           }
           catch (error) {}
