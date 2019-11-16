d4cf707cbe2f2732df9cbe153fc85cce7177d0f5
at1208 <amantiwari12081999@gmail.com>
2019-11-16 07:54:33 +0530

    .

--- src/components/GamesSection/Overwatch2/overwatch2Detail.js
+++ src/components/GamesSection/Overwatch2/overwatch2Detail.js
@@ -20,8 +20,18 @@ const Overwatch2Detail = () => {
    return <div>
          <Header/>
 
+
          <div className='row justify-content-center'>
-         <div className='row justify-content-center col-md-6'>
+         <div className='col-md-12 row justify-content-center container-fluid'>
+       <LeaderBoard />
+        </div>
+       <div className='col-md-12 row justify-content-center'>
+       <div className='row justify-content-center col-md-2 c27'>
+       <MediumRectangle />
+       </div>
+
+
+         <div className='row justify-content-center col-md-7'>
 
 
          <div className='container c21'>
@@ -71,7 +81,7 @@ Overwatch recently came to Nintendo Switch.
      <HalfPageAd />
   </div>
   </div>
-
+ </div>
          </div>
            </div>
 }
